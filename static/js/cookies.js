6641d715612307c8a77b8db2931cc7ea0c891b04
Neealdon3 <138321129+neealdon3@users.noreply.github.com>
2023-10-14 15:40:22 -0700

    Cookies

---
+++ static/js/cookies.js
@@ -0,0 +1,32 @@
+// Create cookie
+function setCookie(cname, cvalue, exdays) {
+	const d = new Date();
+	d.setTime(d.getTime() + (exdays*24*60*60*1000));
+	let expires = "expires="+ d.toUTCString();
+	document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+// Delete cookie
+function deleteCookie(cname) {
+	const d = new Date();
+	d.setTime(d.getTime() + (24*60*60*1000));
+	let expires = "expires="+ d.toUTCString();
+	document.cookie = cname + "=;" + expires + ";path=/";
+}
+
+// Read cookie
+function getCookie(cname) {
+	let name = cname + "=";
+	let decodedCookie = decodeURIComponent(document.cookie);
+	let ca = decodedCookie.split(';');
+	for(let i = 0; i <ca.length; i++) {
+		let c = ca[i];
+		while (c.charAt(0) == ' ') {
+			c = c.substring(1);
+		}
+		if (c.indexOf(name) == 0) {
+			return c.substring(name.length, c.length);
+		}
+	}
+	return "";
+}

37b11a87288f64e46fa01453fc81dfb6274282f9
Neealdon3 <138321129+neealdon3@users.noreply.github.com>
2023-10-14 17:27:14 -0700

    Migerating files

--- static/js/shortkey.js
+++ static/js/shortkey.js
@@ -9,12 +9,12 @@
 				case "h":
 					e.preventDefault();
 					e.stopPropagation();
-					if (document.title === "Hydrogen Hub") {
+					if (document.title === "Hydrogen") {
 						document.title = "Google";
 						document.querySelector("link[rel*='icon']").href = "https://www.gstatic.com/images/branding/googleg/1x/googleg_standard_color_128dp.png";
 					} else {
-						document.title = "Hydrogen Hub";
-						document.querySelector("link[rel*='icon']").href = "/static/gas.png";
+						document.title = "Hydrogen";
+						document.querySelector("link[rel*='icon']").href = "/static/img/gas.png";
 					}
 				default:
 					break;

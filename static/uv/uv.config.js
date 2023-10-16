f4e7ddef11ddd3a8a0d268a970a0751852a1d12b
Neealdon3 <138321129+neealdon3@users.noreply.github.com>
2023-10-16 16:40:13 -0700

    is this the fix

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,5 +1,5 @@
 self.__uv$config = {
-    prefix: '/uv/service/',
+    prefix: '/static/tiw/',
     bare: 'https://phantomnetwork.cloud/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,

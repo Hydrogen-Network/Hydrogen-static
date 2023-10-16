91f78c67936e32edd1b3812306fea346241d0092
Neealdon3 <138321129+neealdon3@users.noreply.github.com>
2023-10-16 16:23:49 -0700

    Yay

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,13 +1,10 @@
-// This file overwrites the stock UV config.js
-
 self.__uv$config = {
-    prefix: "/service/",
-    bare: "https://phantomnetwork.cloud/bare/",
+    prefix: '/uv/service/',
+    bare: 'https://phantomnetwork.cloud/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: "/uv/uv.handler.js",
-    client: "/uv/uv.client.js",
-    bundle: "/uv/uv.bundle.js",
-    config: "/uv/uv.config.js",
-    sw: "/uv/uv.sw.js",
+    handler: '/static/uv/uv.handler.js',
+    bundle: '/static/uv/uv.bundle.js',
+    config: '/static/uv/uv.config.js',
+    sw: '/static/uv/uv.sw.js',
 };

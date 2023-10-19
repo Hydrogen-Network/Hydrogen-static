91486cb9ed9cc6a16dc53107f6f2f0b9e3ba9755
Neealdon3 <138321129+neealdon3@users.noreply.github.com>
2023-10-18 19:07:23 -0700

    help

--- static/js/cloak.js
+++ static/js/cloak.js
@@ -3,8 +3,7 @@
         const option = event.target.value;
 
         if (option === 'blob') {
-          var wait = open('./blob.html');
-          let htmll=wait.document.querySelector("html"),html=htmll.innerHTML;
+          let htmll=document.querySelector("html"),html=htmll.innerHTML;
           html+="</html>",html="<!doctype html> <html> "+html;
           let htmlA=Array(html);
           const blob=new Blob(htmlA,{type:"text/html"});

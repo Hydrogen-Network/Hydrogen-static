a953501187f4c0677f36d0ca252afc469793a72f
Neealdon3 <138321129+neealdon3@users.noreply.github.com>
2023-10-21 09:12:57 -0700

    ok

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,7 +1,7 @@
 /*global Ultraviolet*/
 self.__uv$config = {
     prefix: '/static/tiw/',
-    bare: ['https://stingrayproxy.onrender.com/bare/', 'https://coolbare.lightspeedsucks.workers.dev', 'https://phantomnetwork.cloud/bare/', 'https://baresw.starttiw.org/', 'https://polarislearning.org/bare', 'https://gointerstellar.app/outerspace',  'https://geoquiz.gq/bare/', 'https://tomp.app', 'https://uv.holyubofficial.net/bare1/', 'https://uv.holyubofficial.net/bare2/', 'https://uv.holyubofficial.net/bare3/', 'https://uv.holyubofficial.net/bare4/'],
+    bare: ['https://stingrayproxy.onrender.com/bare/', 'https://coolbare.lightspeedsucks.workers.dev', 'https://phantomnetwork.cloud/bare/', 'https://baresw.starttiw.org/', 'https://polarislearning.org/bare', 'https://geoquiz.gq/bare/', 'https://tomp.app', 'https://uv.holyubofficial.net/bare1/', 'https://uv.holyubofficial.net/bare2/', 'https://uv.holyubofficial.net/bare3/', 'https://uv.holyubofficial.net/bare4/'],
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',

14c34c1187eb570843b21ba6034238e950314b1b
Neealdon3 <138321129+neealdon3@users.noreply.github.com>
2023-10-21 14:20:31 -0700

    ok

--- static/js/main.js
+++ static/js/main.js
@@ -5,137 +5,137 @@ document.addEventListener("DOMContentLoaded", function(event) {
   const name = document.getElementById('dynamic-title');
   var selectedValue = localStorage.getItem("selectedOption");
   if (selectedValue === 'Google') {
-      icon.setAttribute('href', '/images/favicon/google.png');
+      icon.setAttribute('href', '/static/img/cloaks/google.png');
       name.textContent = 'Google';
       localStorage.setItem("name", "Google");
-      localStorage.setItem("icon", "/images/favicon/google.png");
+      localStorage.setItem("icon", "/static/img/cloaks/google.png");
   } 
   else if (selectedValue === 'Drive') {
-      icon.setAttribute('href', '/images/favicon/drive.png');
+      icon.setAttribute('href', '/static/img/cloaks/drive.png');
       name.textContent = 'My Drive - Google Drive';
       localStorage.setItem("name", "My Drive - Google Drive");
-      localStorage.setItem("icon", "/images/favicon/drive.png");
+      localStorage.setItem("icon", "/static/img/cloaks/drive.png");
   } 
   else if (selectedValue === 'Classroom') {
-      icon.setAttribute('href', '/images/favicon/classroom.png');
+      icon.setAttribute('href', '/static/img/cloaks/classroom.png');
       name.textContent = 'Classes';
       localStorage.setItem("name", "Classes");
-      localStorage.setItem("icon", "/images/favicon/classroom.png");
+      localStorage.setItem("icon", "/static/img/cloaks/classroom.png");
   }
   else if (selectedValue === 'Schoology') {
-      icon.setAttribute('href', '/images/favicon/schoology.png');
+      icon.setAttribute('href', '/static/img/cloaks/schoology.png');
       name.textContent = 'Home | Schoology';
       localStorage.setItem("name", "Home | Schoology");
-      localStorage.setItem("icon", "/images/favicon/schoology.png");
+      localStorage.setItem("icon", "/static/img/cloaks/schoology.png");
   }
   else if (selectedValue === 'Gmail') {
-      icon.setAttribute('href', '/images/favicon/gmail.png');
+      icon.setAttribute('href', '/static/img/cloaks/gmail.png');
       name.textContent = 'Gmail';
       localStorage.setItem("name", "Gmail");
-      localStorage.setItem("icon", "/images/favicon/gmail.png");
+      localStorage.setItem("icon", "/static/img/cloaks/gmail.png");
   }
   else if (selectedValue === 'Clever') {
-      icon.setAttribute('href', '/images/favicon/clever.png');
+      icon.setAttribute('href', '/static/img/cloaks/clever.png');
       name.textContent = 'Clever | Portal';
       localStorage.setItem("name", "Clever | Portal");
-      localStorage.setItem("icon", "/images/favicon/clever.png");
+      localStorage.setItem("icon", "/static/img/cloaks/clever.png");
   }
   else if (selectedValue === 'Khan') {
-      icon.setAttribute('href', '/images/favicon/khan.png');
+      icon.setAttribute('href', '/static/img/cloaks/khan.png');
       name.textContent = 'Dashboard | Khan Academy';
       localStorage.setItem("name", "Dashboard | Khan Academy");
-      localStorage.setItem("icon", "/images/favicon/khan.png");
+      localStorage.setItem("icon", "/static/img/cloaks/khan.png");
   }
   var themeid = localStorage.getItem("theme");
   //Loads theme
   themeEle = document.createElement("link");
   themeEle.rel="stylesheet";
   if(themeid == "b") {
-      themeEle.href = "/css/themes/bannana.css";
+      themeEle.href = "/static/css/themes/bannana.css";
   }
   if(themeid == "bg") {
-      themeEle.href = "/css/themes/blue-green.css";
+      themeEle.href = "/static/css/themes/blue-green.css";
   }
   if(themeid == "cr") {
-      themeEle.href = "/css/themes/cherryRed.css";
+      themeEle.href = "/static/css/themes/cherryRed.css";
   }
   if(themeid == "d") {
-      themeEle.href = "/css/themes/dark.css";
+      themeEle.href = "/static/css/themes/dark.css";
   }
   if(themeid == "fg") {
-      themeEle.href = "/css/themes/forestGreen.css";
+      themeEle.href = "/static/css/themes/forestGreen.css";
   }
   if(themeid == "light") {
-      themeEle.href = "/css/theme/solid/light.css";
+      themeEle.href = "/static/css/theme/solid/light.css";
   }
   if(themeid == "m") {
-      themeEle.href = "/css/themes/milkshake.css";
+      themeEle.href = "/static/css/themes/milkshake.css";
   }
   if(themeid == "nb") {
-      themeEle.href = "/css/themes/nightBlue.css";
+      themeEle.href = "/static/css/themes/nightBlue.css";
   }
   if(themeid == "rb") {
-      themeEle.href = "/css/themes/red-black.css";
+      themeEle.href = "/static/css/themes/red-black.css";
   }
   if(themeid == "v4") {
-      themeEle.href = "/css/theme/solid/legacy.css";
+      themeEle.href = "/static/css/theme/solid/legacy.css";
   }
   if(themeid == "midnight") {
-    themeEle.href = "/css/theme/solid/midnight.css";
+    themeEle.href = "/static/css/theme/solid/midnight.css";
   }
   if(themeid == "black-red") {
-    themeEle.href = "/css/theme/gradient/black-red.css";
+    themeEle.href = "/static/css/theme/gradient/black-red.css";
   }
   if(themeid == "black-blue") {
-    themeEle.href = "/css/theme/gradient/black-blue.css";
+    themeEle.href = "/static/css/theme/gradient/black-blue.css";
   }
   if(themeid == "black-green") {
-    themeEle.href = "/css/theme/gradient/black-green.css";
+    themeEle.href = "/static/css/theme/gradient/black-green.css";
   }
   if(themeid == "red") {
-    themeEle.href = "/css/theme/gradient/red.css";
+    themeEle.href = "/static/css/theme/gradient/red.css";
   } 
   if(themeid == "purple") {
-    themeEle.href = "/css/theme/gradient/purple.css";
+    themeEle.href = "/static/css/theme/gradient/purple.css";
   }
   if(themeid == "black-orange") {
-    themeEle.href = "/css/theme/gradient/black-orange.css";
+    themeEle.href = "/static/css/theme/gradient/black-orange.css";
   }
   if(themeid == "ocean-blue") {
-    themeEle.href = "/css/theme/solid/ocean-blue.css";
+    themeEle.href = "/static/css/theme/solid/ocean-blue.css";
   }
   if(themeid == "black-purple") {
-    themeEle.href = "/css/theme/gradient/black-purple.css";
+    themeEle.href = "/static/css/theme/gradient/black-purple.css";
   }
   if(themeid == "black-coral") {
-    themeEle.href = "/css/theme/gradient/black-coral.css";
+    themeEle.href = "/static/css/theme/gradient/black-coral.css";
   }
   if(themeid == "black-ruby") {
-    themeEle.href = "/css/theme/gradient/black-ruby.css";
+    themeEle.href = "/static/css/theme/gradient/black-ruby.css";
   }
   if(themeid == "black-yellow") {
-    themeEle.href = "/css/theme/gradient/black-yellow.css";
+    themeEle.href = "/static/css/theme/gradient/black-yellow.css";
   }
   if(themeid == "black-pink") {
-    themeEle.href = "/css/theme/gradient/black-pink.css";
+    themeEle.href = "/static/css/theme/gradient/black-pink.css";
   }
   if(themeid == "black-white") {
-    themeEle.href = "/css/theme/gradient/black-white.css";
+    themeEle.href = "/static/css/theme/gradient/black-white.css";
   }
   if(themeid == "dark-purple") {
-    themeEle.href = "/css/theme/gradient/dark-purple.css";
+    themeEle.href = "/static/css/theme/gradient/dark-purple.css";
   }
   if(themeid == "sunset") {
-    themeEle.href = "/css/theme/gradient/sunset.css";
+    themeEle.href = "/static/css/theme/gradient/sunset.css";
   }
   if(themeid == "indigo") {
-    themeEle.href = "/css/theme/gradient/indigo.css";
+    themeEle.href = "/static/css/theme/gradient/indigo.css";
   }
   if(themeid == "fire-ice") {
-    themeEle.href = "/css/theme/gradient/fire-ice.css";
+    themeEle.href = "/static/css/theme/gradient/fire-ice.css";
   }
   if(themeid == "purple-blue") {
-    themeEle.href = "/css/theme/gradient/purple-blue.css";
+    themeEle.href = "/static/css/theme/gradient/purple-blue.css";
   }
   document.body.appendChild(themeEle);
 });

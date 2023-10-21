b6ee0f3c83881cf395ad1971dd5ae27aa0a817c3
Neealdon3 <138321129+neealdon3@users.noreply.github.com>
2023-10-20 21:07:12 -0700

    erRegister the Service Wok

--- static/register-sw.js
+++ static/register-sw.js
@@ -1,7 +1,18 @@
 "use strict";
-const stockSW = "/uv/sw.js";
+/**
+ * Distributed with Ultraviolet and compatible with most configurations.
+ */
+const stockSW = "/static/uv-sw.js";
+
+/**
+ * List of hostnames that are allowed to run serviceworkers on http:
+ */
 const swAllowedHostnames = ["localhost", "127.0.0.1"];
 
+/**
+ * Global util
+ * Used in 404.html and index.html
+ */
 async function registerSW() {
   if (
     location.protocol !== "https:" &&
@@ -12,7 +23,8 @@ async function registerSW() {
   if (!navigator.serviceWorker)
     throw new Error("Your browser doesn't support service workers.");
 
+  // Ultraviolet has a stock `sw.js` script.
   await navigator.serviceWorker.register(stockSW, {
     scope: __uv$config.prefix,
-  });  
+  });
 }

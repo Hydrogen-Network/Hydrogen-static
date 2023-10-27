e897405839fcc2f40fd749520ba3371a170c88b3
Neealdon3 <138321129+neealdon3@users.noreply.github.com>
2023-10-26 18:51:01 -0700

    asd

---
+++ static/js/games?.js
@@ -0,0 +1,37 @@
+const gamesContainer = document.querySelector('.gamecontainer');
+
+
+
+/* Adds Element BEFORE NeighborElement */
+Element.prototype.appendBefore = function (element) {
+    element.parentNode.insertBefore(this, element);
+}, false;
+
+/* Adds Element AFTER NeighborElement */
+Element.prototype.appendAfter = function (element) {
+    element.parentNode.insertBefore(this, element.nextSibling);
+}, false;
+
+
+
+// Fetch the games data from a JSON file
+fetch('/static/json/games.json')
+  .then((res) => res.json())
+  .then((games) => {
+    // Loop through each game and create a new game element for it
+    games.forEach((game) => {
+      const gameEl = document.createElement('li');
+      gameEl.innerHTML = `<div class="gamecard"><a href="#"onclick="localStorage.setItem('currentgame', game.url);location.href='play'><img src=game.img class="gameimage"/><div class="gameinfo"><b><p class="gamename"></p></b><p class="gamedesc"></p></div></a></div>`;
+      gamesContainer.appendChild(gameEl);
+
+
+
+
+
+    
+    });
+  })
+  .catch((e) => {
+    alert('Could not load games');
+    alert(e);
+  });

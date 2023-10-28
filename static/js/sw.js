274fab7c76ba830c2daf808695deb301903956b6
Neealdon3 <138321129+neealdon3@users.noreply.github.com>
2023-10-28 11:38:26 -0700

    aaaaaaa

--- static/js/sw.js
+++ static/js/sw.js
@@ -1,8 +1,21 @@
-importScripts('/static/uv/uv.bundle.js');
-importScripts('/static/uv/uv.config.js');
-importScripts('/static/uv/uv.sw.js');
-importScripts('https://arc.io/arc-sw-core.js');
+var cacheName = 'TIWcog';
+var filesToCache = [
+  '/sw.js'
+];
 
-const sw = new UVServiceWorker();
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+  self.skipWaiting();
+});
 
-self.addEventListener('fetch', (event) => event.respondWith(sw.fetch(event)));
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});

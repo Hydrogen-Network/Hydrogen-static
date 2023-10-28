8aa89298a56af37f79b518c388e5932d61d6a5e4
Neealdon3 <138321129+neealdon3@users.noreply.github.com>
2023-10-28 11:35:14 -0700

    aaa

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 /*global Ultraviolet*/
 self.__uv$config = {
-    prefix: '/static/tiw/',
+    prefix: '/uv/service/',
     bare: 'https://tomp.app',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,

867fe797f14663cf75e4a979153f5c57b84e75d1
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2023-10-29 16:36:11 -0700

    a

--- static/js/games.js
+++ static/js/games.js
@@ -22,7 +22,7 @@ fetch('/static/json/games.json')
     games.forEach((game) => {
       const gameEl = document.createElement('li');
       gameEl.innerHTML = `
-      <div class="gamecard" data-category=${game.categories.join(' ')}>
+      <div class="gamecard" data-category="${game.categories.join(' ')}">
           <a href="#" onclick="localStorage.setItem('currentgame', '${game.url}'); location.href='play.html';">
           <img src="${game.img}" class="gameimage"/>
               <div class="gameinfo">

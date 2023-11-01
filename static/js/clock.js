20cfba4aed82a870108c618f814f30cd65764543
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2023-11-01 16:40:25 -0700

    a

--- static/js/clock.js
+++ static/js/clock.js
@@ -1,23 +1,29 @@
-function updateClock() {
-    const now = new Date();
-    let hours = now.getHours();
-    const minutes = now.getMinutes().toString().padStart(2, '0');
-    const seconds = now.getSeconds().toString().padStart(2, '0');
-
-    // Convert hours to 12-hour format
-    if (hours > 12) {
-        hours -= 12;
-    }
-
-    // Ensure that 12:00 is displayed correctly
-    if (hours === 0) {
-        hours = 12;
-    }
-
-    const timeString = `${hours}:${minutes}:${seconds}`;
-    document.getElementsByClassName('.clock').innerHTML = `${hours}:${minutes}:${seconds}`;
+function showTime(){
+  var date = new Date();
+  var h = date.getHours(); // 0 - 23
+  var m = date.getMinutes(); // 0 - 59
+  var s = date.getSeconds(); // 0 - 59
+  var session = "AM";
+  
+  if(h == 0){
+      h = 12;
+  }
+  
+  if(h > 12){
+      h = h - 12;
+      session = "PM";
+  }
+  
+  h = (h < 10) ? "0" + h : h;
+  m = (m < 10) ? "0" + m : m;
+  s = (s < 10) ? "0" + s : s;
+  
+  var time = h + ":" + m + ":" + s + " " + session;
+  document.getElementById("time").innerText = time;
+  document.getElementById("time").textContent = time;
+  
+  setTimeout(showTime, 1000);
+  
 }
 
-// Update the clock immediately and then every second
-updateClock();
-setInterval(updateClock, 1000);
+showTime();

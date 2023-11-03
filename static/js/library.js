a113e2688b4352253b171510945b6bff34170116
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2023-11-03 15:01:39 -0700

    a

--- static/js/library.js
+++ static/js/library.js
@@ -6,41 +6,8 @@ uploadForm.addEventListener('submit', function(event) {
   
   const file = mp3FileInput.files[0];
   
-  // Store the file in IndexedDB
-  const dbName = 'songs';
-  const dbVersion = 1;
-
-  const request = window.indexedDB.open(dbName, dbVersion);
-
-  request.onsuccess = function(event) {
-    const db = event.target.result;
-    const transaction = db.transaction('files', 'readwrite');
-    const objectStore = transaction.objectStore('files');
-    
-    const fileData = {
-      name: file.name,
-      type: file.type,
-      data: file
-    };
-    
-    const addRequest = objectStore.add(fileData);
-    
-    addRequest.onsuccess = function(event) {
-      console.log('File added to IndexedDB');
-    };
-    
-    addRequest.onerror = function(event) {
-      console.error('Error adding file to IndexedDB');
-    };
-    
-    transaction.oncomplete = function(event) {
-      db.close();
-    };
-  };
-  
-  request.onerror = function(event) {
-    console.error('Error opening IndexedDB');
-  };
+  // Store the file in localStorage
+  localStorage.setItem('mp3File', file);
   
   // Clear the file input value
   mp3FileInput.value = '';

45cd18a282fc76f92e8f9e64811f6446921092b9
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2023-11-03 15:06:31 -0700

    a

--- static/js/library.js
+++ static/js/library.js
@@ -7,7 +7,7 @@ uploadForm.addEventListener('submit', function(event) {
   const file = mp3FileInput.files[0];
   
   // Store the file in IndexedDB
-  const dbName = 'myDatabase';
+  const dbName = 'songs';
   const dbVersion = 1;
 
   const request = window.indexedDB.open(dbName, dbVersion);
@@ -45,3 +45,41 @@ uploadForm.addEventListener('submit', function(event) {
   // Clear the file input value
   mp3FileInput.value = '';
 });
+
+
+
+
+// Retrieve the file from IndexedDB
+const dbName = 'songs';
+const dbVersion = 1;
+
+const request = window.indexedDB.open(dbName, dbVersion);
+
+request.onsuccess = function(event) {
+  const db = event.target.result;
+  const transaction = db.transaction('files', 'readonly');
+  const objectStore = transaction.objectStore('files');
+  
+  const getRequest = objectStore.get(1); // Assuming the file is stored with key 1
+  
+  getRequest.onsuccess = function(event) {
+    const fileData = event.target.result;
+    
+    if (fileData) {
+      const audioPlayer = document.getElementById('audioPlayer');
+      audioPlayer.src = URL.createObjectURL(fileData.data);
+    }
+  };
+  
+  getRequest.onerror = function(event) {
+    console.error('Error retrieving file from IndexedDB');
+  };
+  
+  transaction.oncomplete = function(event) {
+    db.close();
+  };
+};
+
+request.onerror = function(event) {
+  console.error('Error opening IndexedDB');
+};

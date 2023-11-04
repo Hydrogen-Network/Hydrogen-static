0b8c8ef6f77bedf3ba95006cb864c9ec6dc1de24
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2023-11-04 14:47:48 -0700

    aaaaaaaaaaaaaaaa

--- static/js/settings.js
+++ static/js/settings.js
@@ -115,20 +115,20 @@ dropdown.addEventListener('change', function() {
 
 const switches = document.getElementById('2');
 
-if(window.localStorage.getItem('v4Particles') != "") {
-  if(window.localStorage.getItem('v4Particles') == "true") {
+if(window.localStorage.getItem('RandomBG') != "") {
+  if(window.localStorage.getItem('RandomBG') == "true") {
     switches.checked = true;
   }
   else {
-    //switches.checked = false;
+    switches.checked = false;
   }
 }
 
 switches.addEventListener('change', (event) => {
   if (event.currentTarget.checked) {
-    window.localStorage.setItem('v4Particles', 'true');
+    window.localStorage.setItem('RandomBG', 'true');
   } else {
-    window.localStorage.setItem('v4Particles', 'false');
+    window.localStorage.setItem('RandomBG', 'false');
   }
 });
 

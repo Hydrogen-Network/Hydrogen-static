882747a734b37b23ffe26d4b179aef167f940c16
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2023-11-04 11:08:14 -0700

    aa

--- static/js/home.js
+++ static/js/home.js
@@ -19,12 +19,12 @@ if (!inFrame && !navigator.userAgent.includes("Firefox")) {
 
         const name = localStorage.getItem("name") || "My Drive - Google Drive";
         const icon = localStorage.getItem("icon") || "https://ssl.gstatic.com/images/branding/product/1x/drive_2020q4_32dp.png";
-        
+
         doc.title = name;
         link.rel = "icon";
         link.href = icon;
-        
-        iframe.src = location.href 
+
+        iframe.src = location.href
         style.position = "fixed"
         style.top = style.bottom = style.left = style.right = 0
         style.border = style.outline = "none"
@@ -36,22 +36,22 @@ if (!inFrame && !navigator.userAgent.includes("Firefox")) {
     }
 }
 
-document.addEventListener("DOMContentLoaded", function(event) { 
-    if(window.localStorage.getItem("v4Particles") == "true") {
-      const scr = document.createElement("script");
-      scr.src="./js/particles.js";
-      document.body.appendChild(scr);
+document.addEventListener("DOMContentLoaded", function(event) {
+    if (window.localStorage.getItem("v4Particles") == "true") {
+        const scr = document.createElement("script");
+        scr.src = "./js/particles.js";
+        document.body.appendChild(scr);
     }
-    if(localStorage.getItem('customcolor') == "true") {
+    if (localStorage.getItem('customcolor') == "true") {
         document.body.style.background = localStorage.getItem("backdrop-color");
     }
     async function randombgs() {
-    if(localStorage.getItem('RandomBG') == "true") {
         var bg = await fetch("/static/js/json/bg.json");
         var bga = await say.json();
-        var randombg = says[Math.floor(Math.random() *  says.length)];
-        document.body.style.background = `url(${randombg})`;
-    }
+        var randombg = says[Math.floor(Math.random() * says.length)];
+        if (localStorage.getItem('RandomBG') == "true") {
+            document.body.style.background = `url(${randombg})`;
+        }
     }
     randombgs();
-  });
+});

dc2b44ef6f92ab6c0488acf2b8b1d9ab4dceda05
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2023-11-04 09:46:05 -0700

    seriously

--- static/js/splashtexts.js
+++ static/js/splashtexts.js
@@ -1,15 +1,18 @@
 async function say(){
-    var say = await fetch("/static/js/json/say.json").json();
-    var randomSplash = say[Math.floor(Math.random() *  say.length)];
+    var say = await fetch("/static/js/json/say.json");
+    var says = say.json();
+    var randomSplash = says[Math.floor(Math.random() *  says.length)];
 
   // If the random message is "%GAMES_NUMBER%", replace it with the number of games available
   if (randomSplash == "%GAMES_NUMBER%") {
-    var games = await fetch(location.origin + "/static/json/games.json").json();
-    randomSplash = "There are " + games.length + " games currently";
+    var games = await fetch(location.origin + "/static/json/games.json");
+    var game = games.json();
+    randomSplash = "There are " + game.length + " games currently";
   }
   // If the random message is "%SPLASH_NUMBER%", replace it with the total number of splash messages
   else if (randomSplash == "%SPLASH_NUMBER%") {
-    var splashCacheAll = await fetch("/static/js/json/say.json").json();
+    var splashCache = await fetch("/static/js/json/say.json");
+    var splashCacheAll = splashCacheAll.json();
     randomSplash = "There are " + splashCacheAll.length + " of these messages!";
   }
 randomSplash =  document.querySelector("#splash").innerHTML = randomSplash;

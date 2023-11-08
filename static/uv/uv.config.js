4f1e03b28e016458057079d11947de5aa41cc88c
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2023-11-07 17:49:03 -0800

    a

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 /*global Ultraviolet*/
 self.__uv$config = {
-    prefix: '/static/hydrogen/',
+    prefix: '/static/tiw/',
     bare: 'https://tomp.app',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,

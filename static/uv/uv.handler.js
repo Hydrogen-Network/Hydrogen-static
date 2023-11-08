2f942b3f8da024fa834f8b1de57cefd79298382d
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2023-11-07 20:04:49 -0800

    aa

--- static/uv/uv.handler.js
+++ static/uv/uv.handler.js
@@ -2,7 +2,7 @@ if (!self.__uv) {
     __uvHook(self, self.__uv$config, self.__uv$config.bare);
 };
 
-async function __uvHook(window, config = {}, bare = '/bare/') {
+async function __uvHook(window, config = {}, bare = '/outerspace/') {
     if ('__uv' in window && window.__uv instanceof Ultraviolet) return false;
 
     if (window.document && !!window.window) {

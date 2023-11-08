72a2b61aa5b832084a3ffdd0832b53da7153998b
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2023-11-07 20:14:31 -0800

    aaaaaaaaaa

--- index.js
+++ index.js
@@ -29,8 +29,6 @@ const routes = [
   { path: '/image-galleries', file: 'play.html' },
 ];
 
-const app = express();
-
 const limiter = rateLimit({
 	windowMs: 15 * 60 * 1000, // 15 minutes
 	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)

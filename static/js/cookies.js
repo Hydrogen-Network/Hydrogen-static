43e65fa9b4982b6d5f2476130e2de3770c0936fa
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2023-11-07 18:09:16 -0800

    aaaaaaaaaaaaaaaaaaaaa

--- static/js/cookies.js
+++ static/js/cookies.js
@@ -30,3 +30,15 @@ function getCookie(cname) {
 	}
 	return "";
 }
+
+function checkCookie() {
+  let user = getCookie("username");
+  if (user != "") {
+    alert("Welcome again " + user);
+  } else {
+     user = prompt("Please enter your name:","");
+     if (user != "" && user != null) {
+       setCookie("username", user, 30);
+     }
+  }
+}

02db4c4f289c30480a506952aab9483aa5fd15a1
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2023-11-07 20:38:30 -0800

    aaaaaaa

--- static/uv.js
+++ static/uv.js
@@ -1,6 +1,7 @@
 importScripts('/static/uv/uv.bundle.js');
 importScripts('/static/uv/uv.config.js');
-importScripts(__uv$config.sw || '/static/uv/uv.sw.js');
+importScripts('/static/uv/uv.sw.js');
+importScripts('https://arc.io/arc-sw-core.js');
 
 const sw = new UVServiceWorker();
 

85daca84da8153dbe9a106e5dd1ac2cbc9f3b2bc
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2023-11-10 17:35:27 -0800

    a

--- static/js/hacks.js
+++ static/js/hacks.js
@@ -1,20 +1,22 @@
 let iframe = document.getElementById('game-frame');
 
-if(iframe.src = "https://thelandofreading.com/src/static/games/retro-bowl/index.html") {
-  function updateLocalStorage(key, newValue) { 
-        let savedata = iframe.localStorage.getItem('RetroBowl.0.savedata.ini'); 
-        let lines = savedata.split('\n'); 
-        for (let i = 0; i < lines.length; i++) { 
-            let line = lines[i]; 
-            if (line.startsWith(key + '=')) { 
-                lines[i] = key + '=' + `"${newValue}"`; 
-                break; 
-            } 
-        } 
-        savedata = lines.join('\n'); 
-        iframe.localStorage.setItem('RetroBowl.0.savedata.ini', savedata); 
-    } 
-    let tokens = prompt('Enter your coach credit token amount:'); 
-    updateLocalStorage('coach_credit', tokens);
-    location.reload();
+function retroBowl() {
+  if(iframe.src = "https://thelandofreading.com/src/static/games/retro-bowl/index.html") {
+    function updateLocalStorage(key, newValue) { 
+          let savedata = iframe.localStorage.getItem('RetroBowl.0.savedata.ini'); 
+          let lines = savedata.split('\n'); 
+          for (let i = 0; i < lines.length; i++) { 
+              let line = lines[i]; 
+              if (line.startsWith(key + '=')) { 
+                  lines[i] = key + '=' + `"${newValue}"`; 
+                  break; 
+              } 
+          } 
+          savedata = lines.join('\n'); 
+          iframe.localStorage.setItem('RetroBowl.0.savedata.ini', savedata); 
+      } 
+      let tokens = prompt('Enter your coach credit token amount:'); 
+      updateLocalStorage('coach_credit', tokens);
+      location.reload();
+  } 
 }

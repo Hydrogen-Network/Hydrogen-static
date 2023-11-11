d12ff8922ae295f616336adb64e71e2571badb88
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2023-11-10 17:41:31 -0800

    a

--- static/js/hacks.js
+++ static/js/hacks.js
@@ -3,20 +3,20 @@ let iframe = document.getElementById('game-frame');
 function retroBowl() {
   if(iframe.src = "https://thelandofreading.com/src/static/games/retro-bowl/index.html") {
     function updateLocalStorage(key, newValue) { 
-          let savedata = localStorage.getItem('RetroBowl.0.savedata.ini'); 
-          let lines = savedata.split('\n'); 
-          for (let i = 0; i < lines.length; i++) { 
-              let line = lines[i]; 
-              if (line.startsWith(key + '=')) { 
-                  lines[i] = key + '=' + `"${newValue}"`; 
-                  break; 
-              } 
-          } 
-          savedata = lines.join('\n'); 
-          localStorage.setItem('RetroBowl.0.savedata.ini', savedata); 
-      } 
-      let tokens = prompt('Enter your coach credit token amount:'); 
-      updateLocalStorage('coach_credit', tokens);
-      location.reload();
+        let savedata = localStorage.getItem('RetroBowl.0.savedata.ini'); 
+        let lines = savedata.split('\n'); 
+        for (let i = 0; i < lines.length; i++) { 
+            let line = lines[i]; 
+            if (line.startsWith(key + '=')) { 
+                lines[i] = key + '=' + `"${newValue}"`; 
+                break; 
+            } 
+        } 
+        savedata = lines.join('\n'); 
+        localStorage.setItem('RetroBowl.0.savedata.ini', savedata); 
+    } 
+    let tokens = prompt('Enter your coach credit token amount:'); 
+    updateLocalStorage('coach_credit', tokens);
+    alert("Now refresh and your credits will be there.")
   } 
 }

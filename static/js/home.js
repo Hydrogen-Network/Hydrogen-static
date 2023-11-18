360b18ea5494eb7d0b1d2a97e547f0b727c8d9f8
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2023-11-17 16:00:09 -0800

    a

--- static/js/home.js
+++ static/js/home.js
@@ -1,4 +1,4 @@
-/*
+
 let inFrame
 
 try {
@@ -8,35 +8,37 @@ try {
 }
 
 // Cloaking Code
-if (!inFrame && !navigator.userAgent.includes("Firefox")) {
-    const popup = open("about:blank", "_blank")
-    if (!popup || popup.closed) {
-        alert("Please allow popups and redirects.")
-    } else {
-        const doc = popup.document
-        const iframe = doc.createElement("iframe")
-        const style = iframe.style
-        const link = doc.createElement("link")
+if (window.location.href) {
+    if (!inFrame && !navigator.userAgent.includes("Firefox")) {
+        const popup = open("about:blank", "_blank")
+        if (!popup || popup.closed) {
+            alert("Please allow popups and redirects.")
+        } else {
+            const doc = popup.document
+            const iframe = doc.createElement("iframe")
+            const style = iframe.style
+            const link = doc.createElement("link")
 
-        const name = localStorage.getItem("name") || "My Drive - Google Drive";
-        const icon = localStorage.getItem("icon") || "https://ssl.gstatic.com/images/branding/product/1x/drive_2020q4_32dp.png";
+            const name = localStorage.getItem("name") || "My Drive - Google Drive";
+            const icon = localStorage.getItem("icon") || "https://ssl.gstatic.com/images/branding/product/1x/drive_2020q4_32dp.png";
 
-        doc.title = name;
-        link.rel = "icon";
-        link.href = icon;
+            doc.title = name;
+            link.rel = "icon";
+            link.href = icon;
 
-        iframe.src = location.href
-        style.position = "fixed"
-        style.top = style.bottom = style.left = style.right = 0
-        style.border = style.outline = "none"
-        style.width = style.height = "100%"
+            iframe.src = location.href
+            style.position = "fixed"
+            style.top = style.bottom = style.left = style.right = 0
+            style.border = style.outline = "none"
+            style.width = style.height = "100%"
 
-        doc.head.appendChild(link);
-        doc.body.appendChild(iframe)
-        location.replace("https://www.nasa.gov/")
+            doc.head.appendChild(link);
+            doc.body.appendChild(iframe)
+            location.replace("https://www.nasa.gov/")
+        }
     }
 }
-*/
+
 
 document.addEventListener("DOMContentLoaded", function(event) {
     async function start() {
@@ -61,4 +63,22 @@ document.addEventListener("DOMContentLoaded", function(event) {
         }   
     }
     start();
+});
+
+document.addEventListener('keydown', function (event) {
+      console.log('Keydown event:', event);
+      const target = event.target;
+      const customCSS = localStorage.getItem('emergencyHotkey');
+      if (customCSS && (!target || (target.nodeName !== 'INPUT' || target.getAttribute('id') !== 'uv-address'))) {
+        const pressedKey = event.key.toLowerCase();
+        const emergencyHotkey = localStorage.getItem('emergencyHotkey');
+  
+        if (pressedKey === emergencyHotkey) {
+          const emergencyURL = localStorage.getItem('emergencyURL');
+  
+          if (emergencyURL) {
+            window.location.href = emergencyURL;
+          }
+        }
+      }
 });

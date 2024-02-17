dc8089f0d3e39b3105dbd044eb616b3b3ee69e30
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2024-02-17 14:18:26 -0800

    Update apps.js

--- static/js/apps.js
+++ static/js/apps.js
@@ -1,68 +1,46 @@
-const appsContainer = document.querySelector('.appcontainer');
+const gamesContainer = document.querySelector('.gamecontainer');
 
-// Fetch the apps data from a JSON file
+async function loadgame() {
+// Fetch the games data from a JSON file
 fetch('/static/js/json/apps.json')
   .then((res) => res.json())
-  .then((apps.sort((a, b) => a.name.localeCompare(b.name))) => {
-    // Loop through each app and create a new app element for it
-    apps.forEach((app) => {
-      const appEl = document.createElement('li');
-      appEl.innerHTML = `
-      <div class="appcard" data-category="${app.categories}">
-          <a href="#" onclick="localStorage.setItem('currentgame', '${app.url}'); localStorage.setItem('currentgamename', '${app.name}'); localStorage.setItem('currentgamecheat', '${app.cheat}'); location.href='play.html';">
-          <img src="${app.img}" class="gameimage"/>
+  .then((games.sort((a, b) => a.name.localeCompare(b.name))) => {
+    // Loop through each game and create a new game element for it
+    games.forEach((game) => {
+      const gameEl = document.createElement('li');
+      gameEl.innerHTML = `
+      <div class="gamecard" data-category="${game.categories}">
+          <a href="#" onclick="localStorage.setItem('currentgame', '${game.url}'); localStorage.setItem('currentgamename', '${game.name}'); localStorage.setItem('currentgamecheat', '${game.cheat}'); location.href='play.html';">
+          <img title='${game.name}' src="${game.img}" class="gameimage"/>
               <div class="gameinfo">
                   <b>
-                      <p class="appname">${app.name}</p>
+                      <p class="gamename">${game.name}</p>
                   </b>
-                      <p class="appdesc">${app.desc}</p>
+                      <p class="gamedesc">${game.desc}</p>
               </div>
           </a>
       </div>
       `;
-      document.getElementById('appscontainer').appendChild(appEl);
+      document.querySelector('.gamecontainer').appendChild(gameEl);
     });
   })
   .catch((e) => {
-    console.error('Could not load apps. '+e);
+    console.error('Could not load games. '+e);
   });
+}
 
 
 function showImages() {
   var selectedCategories = Array.from(document.querySelectorAll("#category option:checked")).map(option => option.value);
-  var apps = document.getElementsByClassName("appcard");
+  var games = document.getElementsByClassName("gamecard");
 
   for (var i = 0; i < games.length; i++) {
-    var app = apps[i];
-    var categories = app.getAttribute("data-category").split(" ");
+    var game = games[i];
+    var categories = game.getAttribute("data-category").split(" ");
     if (selectedCategories.length === 0 || selectedCategories.some(category => categories.includes(category))) {
       game.style.display = "block";
     } else {
       game.style.display = "none";
     }
   }
-}
-
-function setPin(index) {
-  pins = localStorage.getItem("pinnedGames");
-  if(pins == null) {
-    pins = [];
-  }
-  if(pins == "") {
-    pins = [];
-  }
-  else {
-    pins = pins.split(",").map(Number);
-  }
-  if(pinContains(index,pins)) {
-    let remove = pins.indexOf(index);
-
-    pins.splice(remove, 1);
-
-  }
-  else {
-    pins.push(index);
-  }
-  localStorage.setItem("pinnedGames", pins);
-  location.reload();
 }

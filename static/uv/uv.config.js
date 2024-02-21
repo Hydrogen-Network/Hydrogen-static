cf5c58a7efe5667c8f27f2a502ea0d660e6052a3
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2024-02-20 19:06:11 -0800

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -8,14 +8,14 @@ self.__uv$config = {
     bundle: 'uv/uv.bundle.js',
     config: 'uv/uv.config.js',
     sw: 'uv/uv.sw.js',
-//    inject: async (url) => {
-  //      if (url.host === 'discord.com') {
-    //        return `
-      //          <script src="https://raw.githubusercontent.com/Vencord/builds/main/browser.js"></script>
-        //        <link rel="stylesheet" href="https://raw.githubusercontent.com/Vencord/builds/main/browser.css">
-         //     `;
-        //}//
+    inject: async (url) => {
+       if (url.host === 'discord.com') {
+           return `
+               <script src="https://raw.githubusercontent.com/Vencord/builds/main/browser.js"></script>
+               <link rel="stylesheet" href="https://raw.githubusercontent.com/Vencord/builds/main/browser.css">
+             `;
+        }
 
-       // return ``;
-    //},
+       return ``;
+    },
 };

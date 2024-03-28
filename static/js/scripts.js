880e839f6fadc7137f799882006c22d2fc768fb2
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2024-03-28 14:38:32 -0700

    Create scripts.js

---
+++ static/js/scripts.js
@@ -0,0 +1,32 @@
+function script(text) {
+  console.log("%cScript Injection", "color: cyan; font-weight: 600; background: black; padding: 0 5px; border-radius: 5px", text);
+}
+
+// ====================================
+// SCRIPT INJECTION
+// ====================================
+const gogascript27 = document.createElement("script");
+gogascript27.setAttribute("async", "");
+gogascript27.setAttribute("src", "https://www.googletagmanager.com/gtag/js?id=G-Y1GKR3GL1R");
+const inlinegogascript843 = document.createElement("script");
+inlinegogascript843.innerHTML = `window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-Y1GKR3GL1R');`;
+document.head.append(gogascript27, inlinegogascript843);
+script("Injected script 1/3 (Google Tag Manager)");
+
+const adblockhandle44 = document.createElement("script");
+adblockhandle44.setAttribute("src", "https://fundingchoicesmessages.google.com/i/pub-5756835229788588?ers=1");
+adblockhandle44.setAttribute("nonce", "yibq-w_TR5NOCRWsU-VL0Q");
+adblockhandle44.setAttribute("async", "");
+document.head.append(adblockhandle44);
+script("Injected script 2/3 (Ad stuff)");
+
+const adscipterz92 = document.createElement("script");
+adscipterz92.setAttribute("async", "");
+adscipterz92.setAttribute("src", "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3510278623917680");
+adscipterz92.setAttribute("crossorigin", "anonymous");
+document.head.append(adscipterz92);
+script("Injected script 3/3 (Ad stuff)");
+

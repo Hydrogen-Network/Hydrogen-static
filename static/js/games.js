6af9d11154fde40f91d0f368d8be25136f6422a9
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2024-03-29 04:50:15 +0000

    Update games.js

--- static/js/games.js
+++ static/js/games.js
@@ -45,53 +45,6 @@ fetch('/static/js/json/games.json')
     });
   })
  
-// Fetch the games data from a JSON file
-fetch('/static/js/json/games.json')
-  .then((res) => res.json())
-  .then((games) => {
-    //games.sort((a, b) => {
-    //  if (a.isPinned && !b.isPinned) return -1;
-    //  if (!a.isPinned && b.isPinned return 1;
-    //  return a.name.localeCompare(b.name);
-    //});
-    // Loop through each game and create a new game element for it
-    games.forEach((game) => {
-      if(localStorage.getItem(game.name) == "pinned") {
-        const gameEl = document.createElement('li');
-        gameEl.innerHTML = `
-        <div class="gamecard" data-category="${game.categories}">
-            <a href="#" onclick="localStorage.setItem('currentgame', '${game.url}'); localStorage.setItem('currentgamename', '${game.name}'); localStorage.setItem('currentgamecheat', '${game.cheat}'); location.href='play.html';">
-            <img title='${game.name}' src="${game.img}" class="gameimage"/>
-                <div class="gameinfo">
-                    <b>
-                        <p class="gamename">${game.name}</p>
-                    </b>
-                        <p class="gamedesc">${game.desc}</p>
-                </div>
-            </a>
-        </div>
-        `;
-        document.querySelector('.pinned').appendChild(gameEl);
-      }
-      
-      const gameEl = document.createElement('li');
-      gameEl.innerHTML = `
-      <div class="gamecard" data-category="${game.categories}">
-          <a href="#" onclick="localStorage.setItem('currentgame', '${game.url}'); localStorage.setItem('currentgamename', '${game.name}'); localStorage.setItem('currentgamecheat', '${game.cheat}'); location.href='play.html';">
-          <img title='${game.name}' src="${game.img}" class="gameimage"/>
-              <div class="gameinfo">
-                  <b>
-                      <p class="gamename">${game.name}</p>
-                  </b>
-                      <p class="gamedesc">${game.desc}</p>
-              </div>
-          </a>
-      </div>
-      `;
-      document.querySelector('.gamecontainer').appendChild(gameEl);
-    });
-  })
-
 function showImages() {
   var selectedCategories = Array.from(document.querySelectorAll("#category option:checked")).map(option => option.value);
   var games = document.getElementsByClassName("gamecard");

44680f20588a8e2321dfec3cdcc63ef868dfa5a2
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2024-03-30 04:44:38 +0000

    Update apps.js

--- static/js/apps.js
+++ static/js/apps.js
@@ -3,15 +3,41 @@ const appsContainer = document.querySelector('.appcontainer');
 // Fetch the apps data from a JSON file
 fetch('/static/js/json/apps.json')
   .then((res) => res.json())
-  //.then((apps.sort((a, b) => a.name.localeCompare(b.name))) => {
-  .then((apps)) => {
+  .then((apps) => {
+    apps.sort(function (a, b) {
+      return a.name.localeCompare(b.name);
+    });
     // Loop through each app and create a new app element for it
     apps.forEach((app) => {
+      if(localStorage.getItem(app.name) == "pinned") {
+        const gasmeEl = document.createElement('li');
+        gasmeEl.innerHTML = `
+        <div class="appcard" data-category="${app.categories}">
+            <a href="#" onclick="localStorage.setItem('currentapp', '${app.url}'); localStorage.setItem('currentappname', '${app.name}'); localStorage.setItem('currentappcheat', '${app.cheat}'); location.href='play.html';">
+            <img title='${app.name}' src="${app.img}" class="appimage"/>
+            </a>
+            <i onclick="pin('${app.name}');" style="color:white;" class="fa fa-map-pin" aria-hidden="true"></i>
+            <a href="#" onclick="localStorage.setItem('currentapp', '${app.url}'); localStorage.setItem('currentappname', '${app.name}'); localStorage.setItem('currentappcheat', '${app.cheat}'); location.href='play.html';">
+                <div class="appinfo">
+                    <b>
+                        <p class="appname">${app.name}</p>
+                    </b>
+                        <p class="appdesc">${app.desc}</p>
+                </div>
+            </a>
+        </div>
+        `;
+        document.querySelector('.pinned').appendChild(gasmeEl);
+      }
+      
       const appEl = document.createElement('li');
       appEl.innerHTML = `
       <div class="appcard" data-category="${app.categories}">
           <a href="#" onclick="localStorage.setItem('currentapp', '${app.url}'); localStorage.setItem('currentappname', '${app.name}'); localStorage.setItem('currentappcheat', '${app.cheat}'); location.href='play.html';">
           <img title='${app.name}' src="${app.img}" class="appimage"/>
+          </a>
+          <i onclick="pin('${app.name}');" style="color:white;" class="fa fa-map-pin" aria-hidden="true"></i>
+          <a href="#" onclick="localStorage.setItem('currentapp', '${app.url}'); localStorage.setItem('currentappname', '${app.name}'); localStorage.setItem('currentappcheat', '${app.cheat}'); location.href='play.html';">
               <div class="appinfo">
                   <b>
                       <p class="appname">${app.name}</p>
@@ -24,12 +50,59 @@ fetch('/static/js/json/apps.json')
       document.querySelector('.appcontainer').appendChild(appEl);
     });
   })
-  .catch((e) => {
-    console.error('Could not load apps. '+e);
-  });
-
-
-
+  .catch(error => {
+    // Fetch the apps data from a JSON file
+fetch('/static/js/json/apps.json')
+  .then((res) => res.json())
+  .then((apps) => {
+    apps.sort(function (a, b) {
+      return a.name.localeCompare(b.name);
+    });
+    // Loop through each app and create a new app element for it
+    apps.forEach((app) => {
+      if(localStorage.getItem(app.name) == "pinned") {
+        const gasmeEl = document.createElement('li');
+        gasmeEl.innerHTML = `
+        <div class="appcard" data-category="${app.categories}">
+            <a href="#" onclick="localStorage.setItem('currentapp', '${app.url}'); localStorage.setItem('currentappname', '${app.name}'); localStorage.setItem('currentappcheat', '${app.cheat}'); location.href='play.html';">
+            <img title='${app.name}' src="${app.img}" class="appimage"/>
+            </a>
+            <i onclick="pin('${app.name}');" style="color:white;" class="fa fa-map-pin" aria-hidden="true"></i>
+            <a href="#" onclick="localStorage.setItem('currentapp', '${app.url}'); localStorage.setItem('currentappname', '${app.name}'); localStorage.setItem('currentappcheat', '${app.cheat}'); location.href='play.html';">
+                <div class="appinfo">
+                    <b>
+                        <p class="appname">${app.name}</p>
+                    </b>
+                        <p class="appdesc">${app.desc}</p>
+                </div>
+            </a>
+        </div>
+        `;
+        document.querySelector('.pinned').appendChild(gasmeEl);
+      }
+      
+      const appEl = document.createElement('li');
+      appEl.innerHTML = `
+      <div class="appcard" data-category="${app.categories}">
+          <a href="#" onclick="localStorage.setItem('currentapp', '${app.url}'); localStorage.setItem('currentappname', '${app.name}'); localStorage.setItem('currentappcheat', '${app.cheat}'); location.href='play.html';">
+          <img title='${app.name}' src="${app.img}" class="appimage"/>
+          </a>
+          <i onclick="pin('${app.name}');" style="color:white;" class="fa fa-map-pin" aria-hidden="true"></i>
+          <a href="#" onclick="localStorage.setItem('currentapp', '${app.url}'); localStorage.setItem('currentappname', '${app.name}'); localStorage.setItem('currentappcheat', '${app.cheat}'); location.href='play.html';">
+              <div class="appinfo">
+                  <b>
+                      <p class="appname">${app.name}</p>
+                  </b>
+                      <p class="appdesc">${app.desc}</p>
+              </div>
+          </a>
+      </div>
+      `;
+      document.querySelector('.appcontainer').appendChild(appEl);
+    });
+  })
+  })
+ 
 function showImages() {
   var selectedCategories = Array.from(document.querySelectorAll("#category option:checked")).map(option => option.value);
   var apps = document.getElementsByClassName("appcard");
@@ -42,5 +115,13 @@ function showImages() {
     } else {
       app.style.display = "none";
     }
+  }
+}
+
+function pin(name)  {
+  if (localStorage.getItem(name) == "pinned") {
+    localStorage.setItem(name, "");
+  } else {
+    localStorage.setItem(name, "pinned");
   }
 }

d688cf936649e7d399f9d6f997bdaea6c690e0aa
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2024-03-30 10:32:18 -0700

    Update apps.js

--- static/js/apps.js
+++ static/js/apps.js
@@ -1,119 +1,119 @@
-const appsContainer = document.querySelector('.appcontainer');
+const gamesContainer = document.querySelector('.gamecontainer');
 
 // Fetch the apps data from a JSON file
-fetch('/static/js/json/apps.json')
+fetch('/static/js/json/games.json')
   .then((res) => res.json())
-  .then((apps) => {
-    apps.sort(function (a, b) {
+  .then((games) => {
+    games.sort(function (a, b) {
       return a.name.localeCompare(b.name);
     });
-    // Loop through each app and create a new app element for it
-    apps.forEach((app) => {
-      if(localStorage.getItem(app.name) == "pinned") {
+    // Loop through each game and create a new game element for it
+    games.forEach((game) => {
+      if(localStorage.getItem(game.name) == "pinned") {
         const gasmeEl = document.createElement('li');
         gasmeEl.innerHTML = `
-        <div class="appcard" data-category="${app.categories}">
-            <a href="#" onclick="localStorage.setItem('currentapp', '${app.url}'); localStorage.setItem('currentappname', '${app.name}'); localStorage.setItem('currentappcheat', '${app.cheat}'); location.href='play.html';">
-            <img title='${app.name}' src="${app.img}" class="appimage"/>
+        <div class="gamecard" data-category="${game.categories}">
+            <a href="#" onclick="localStorage.setItem('currentgame', '${game.url}'); localStorage.setItem('currentgamename', '${game.name}'); localStorage.setItem('currentgamecheat', '${game.cheat}'); location.href='play.html';">
+            <img title='${game.name}' src="${game.img}" class="gameimage"/>
             </a>
-            <i onclick="pin('${app.name}');" style="color:white;" class="fa fa-map-pin" aria-hidden="true"></i>
-            <a href="#" onclick="localStorage.setItem('currentapp', '${app.url}'); localStorage.setItem('currentappname', '${app.name}'); localStorage.setItem('currentappcheat', '${app.cheat}'); location.href='play.html';">
-                <div class="appinfo">
+            <i onclick="pin('${game.name}');" style="color:white;" class="fa fa-map-pin" aria-hidden="true"></i>
+            <a href="#" onclick="localStorage.setItem('currentgame', '${game.url}'); localStorage.setItem('currentgamename', '${game.name}'); localStorage.setItem('currentgamecheat', '${game.cheat}'); location.href='play.html';">
+                <div class="gameinfo">
                     <b>
-                        <p class="appname">${app.name}</p>
+                        <p class="gamename">${game.name}</p>
                     </b>
-                        <p class="appdesc">${app.desc}</p>
+                        <p class="gamedesc">${game.desc}</p>
                 </div>
             </a>
         </div>
         `;
-        document.querySelector('.pinned').appendChild(gasmeEl);
+        document.querySelector('.pinned').gameendChild(gasmeEl);
       }
       
-      const appEl = document.createElement('li');
-      appEl.innerHTML = `
-      <div class="appcard" data-category="${app.categories}">
-          <a href="#" onclick="localStorage.setItem('currentapp', '${app.url}'); localStorage.setItem('currentappname', '${app.name}'); localStorage.setItem('currentappcheat', '${app.cheat}'); location.href='play.html';">
-          <img title='${app.name}' src="${app.img}" class="appimage"/>
+      const gameEl = document.createElement('li');
+      gameEl.innerHTML = `
+      <div class="gamecard" data-category="${game.categories}">
+          <a href="#" onclick="localStorage.setItem('currentgame', '${game.url}'); localStorage.setItem('currentgamename', '${game.name}'); localStorage.setItem('currentgamecheat', '${game.cheat}'); location.href='play.html';">
+          <img title='${game.name}' src="${game.img}" class="gameimage"/>
           </a>
-          <i onclick="pin('${app.name}');" style="color:white;" class="fa fa-map-pin" aria-hidden="true"></i>
-          <a href="#" onclick="localStorage.setItem('currentapp', '${app.url}'); localStorage.setItem('currentappname', '${app.name}'); localStorage.setItem('currentappcheat', '${app.cheat}'); location.href='play.html';">
-              <div class="appinfo">
+          <i onclick="pin('${game.name}');" style="color:white;" class="fa fa-map-pin" aria-hidden="true"></i>
+          <a href="#" onclick="localStorage.setItem('currentgame', '${game.url}'); localStorage.setItem('currentgamename', '${game.name}'); localStorage.setItem('currentgamecheat', '${game.cheat}'); location.href='play.html';">
+              <div class="gameinfo">
                   <b>
-                      <p class="appname">${app.name}</p>
+                      <p class="gamename">${game.name}</p>
                   </b>
-                      <p class="appdesc">${app.desc}</p>
+                      <p class="gamedesc">${game.desc}</p>
               </div>
           </a>
       </div>
       `;
-      document.querySelector('.appcontainer').appendChild(appEl);
+      document.querySelector('.gamecontainer').gameendChild(gameEl);
     });
   })
   .catch(error => {
-    // Fetch the apps data from a JSON file
-fetch('/static/js/json/apps.json')
+    // Fetch the games data from a JSON file
+fetch('/static/js/json/games.json')
   .then((res) => res.json())
-  .then((apps) => {
-    apps.sort(function (a, b) {
+  .then((games) => {
+    games.sort(function (a, b) {
       return a.name.localeCompare(b.name);
     });
-    // Loop through each app and create a new app element for it
-    apps.forEach((app) => {
-      if(localStorage.getItem(app.name) == "pinned") {
+    // Loop through each game and create a new game element for it
+    games.forEach((game) => {
+      if(localStorage.getItem(game.name) == "pinned") {
         const gasmeEl = document.createElement('li');
         gasmeEl.innerHTML = `
-        <div class="appcard" data-category="${app.categories}">
-            <a href="#" onclick="localStorage.setItem('currentapp', '${app.url}'); localStorage.setItem('currentappname', '${app.name}'); localStorage.setItem('currentappcheat', '${app.cheat}'); location.href='play.html';">
-            <img title='${app.name}' src="${app.img}" class="appimage"/>
+        <div class="gamecard" data-category="${game.categories}">
+            <a href="#" onclick="localStorage.setItem('currentgame', '${game.url}'); localStorage.setItem('currentgamename', '${game.name}'); localStorage.setItem('currentgamecheat', '${game.cheat}'); location.href='play.html';">
+            <img title='${game.name}' src="${game.img}" class="gameimage"/>
             </a>
-            <i onclick="pin('${app.name}');" style="color:white;" class="fa fa-map-pin" aria-hidden="true"></i>
-            <a href="#" onclick="localStorage.setItem('currentapp', '${app.url}'); localStorage.setItem('currentappname', '${app.name}'); localStorage.setItem('currentappcheat', '${app.cheat}'); location.href='play.html';">
-                <div class="appinfo">
+            <i onclick="pin('${game.name}');" style="color:white;" class="fa fa-map-pin" aria-hidden="true"></i>
+            <a href="#" onclick="localStorage.setItem('currentgame', '${game.url}'); localStorage.setItem('currentgamename', '${game.name}'); localStorage.setItem('currentgamecheat', '${game.cheat}'); location.href='play.html';">
+                <div class="gameinfo">
                     <b>
-                        <p class="appname">${app.name}</p>
+                        <p class="gamename">${game.name}</p>
                     </b>
-                        <p class="appdesc">${app.desc}</p>
+                        <p class="gamedesc">${game.desc}</p>
                 </div>
             </a>
         </div>
         `;
-        document.querySelector('.pinned').appendChild(gasmeEl);
+        document.querySelector('.pinned').gameendChild(gasmeEl);
       }
       
-      const appEl = document.createElement('li');
-      appEl.innerHTML = `
-      <div class="appcard" data-category="${app.categories}">
-          <a href="#" onclick="localStorage.setItem('currentapp', '${app.url}'); localStorage.setItem('currentappname', '${app.name}'); localStorage.setItem('currentappcheat', '${app.cheat}'); location.href='play.html';">
-          <img title='${app.name}' src="${app.img}" class="appimage"/>
+      const gameEl = document.createElement('li');
+      gameEl.innerHTML = `
+      <div class="gamecard" data-category="${game.categories}">
+          <a href="#" onclick="localStorage.setItem('currentgame', '${game.url}'); localStorage.setItem('currentgamename', '${game.name}'); localStorage.setItem('currentgamecheat', '${game.cheat}'); location.href='play.html';">
+          <img title='${game.name}' src="${game.img}" class="gameimage"/>
           </a>
-          <i onclick="pin('${app.name}');" style="color:white;" class="fa fa-map-pin" aria-hidden="true"></i>
-          <a href="#" onclick="localStorage.setItem('currentapp', '${app.url}'); localStorage.setItem('currentappname', '${app.name}'); localStorage.setItem('currentappcheat', '${app.cheat}'); location.href='play.html';">
-              <div class="appinfo">
+          <i onclick="pin('${game.name}');" style="color:white;" class="fa fa-map-pin" aria-hidden="true"></i>
+          <a href="#" onclick="localStorage.setItem('currentgame', '${game.url}'); localStorage.setItem('currentgamename', '${game.name}'); localStorage.setItem('currentgamecheat', '${game.cheat}'); location.href='play.html';">
+              <div class="gameinfo">
                   <b>
-                      <p class="appname">${app.name}</p>
+                      <p class="gamename">${game.name}</p>
                   </b>
-                      <p class="appdesc">${app.desc}</p>
+                      <p class="gamedesc">${game.desc}</p>
               </div>
           </a>
       </div>
       `;
-      document.querySelector('.appcontainer').appendChild(appEl);
+      document.querySelector('.gamecontainer').gameendChild(gameEl);
     });
   })
   })
  
 function showImages() {
   var selectedCategories = Array.from(document.querySelectorAll("#category option:checked")).map(option => option.value);
-  var apps = document.getElementsByClassName("appcard");
+  var games = document.getElementsByClassName("gamecard");
 
-  for (var i = 0; i < apps.length; i++) {
-    var app = apps[i];
-    var categories = app.getAttribute("data-category").split(" ");
+  for (var i = 0; i < games.length; i++) {
+    var game = games[i];
+    var categories = game.getAttribute("data-category").split(" ");
     if (selectedCategories.length === 0 || selectedCategories.some(category => categories.includes(category))) {
-      app.style.display = "block";
+      game.style.display = "block";
     } else {
-      app.style.display = "none";
+      game.style.display = "none";
     }
   }
 }

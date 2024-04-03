dc31211f238256f6343c7100d07c8c3614e7bc8b
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2024-04-03 14:24:17 -0700

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,7 +1,7 @@
 /*global Ultraviolet*/
 self.__uv$config = {
     prefix: '/static/hydrogen/',
-    bare:  'https://worker-holy-smoke-5eb3.pres1234569.workers.dev/',
+    bare:  'https://bare.benrogo.net',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: 'uv/uv.handler.js',

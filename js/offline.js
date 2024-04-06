b097951c573e3a99785435416be54af78bd9e197
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2024-04-06 14:17:04 -0700

    Create offline.js

---
+++ js/offline.js
@@ -0,0 +1,33 @@
+(function() {
+
+  var allNavItems = $(".main-nav li");
+  var allTabs = $(".tab");
+
+  allNavItems.on("click", function(e) {
+
+    e.preventDefault();
+
+    allNavItems.removeClass("active");
+    allTabs.removeClass("active");
+
+    // this = list item
+    $(this).addClass("active");
+
+    var selector = $(this).find("a").attr("href");
+    $(selector).addClass("active");
+
+  });
+
+  // ServiceWorker is a progressive technology. Ignore unsupported browsers
+  if ('serviceWorker' in navigator) {
+    console.log('CLIENT: service worker registration in progress.');
+    navigator.serviceWorker.register('./service-worker.js').then(function() {
+      console.log('CLIENT: service worker registration complete.');
+    }, function() {
+      console.log('CLIENT: service worker registration failure.');
+    });
+  } else {
+    console.log('CLIENT: service worker is not supported.');
+  }
+
+})();

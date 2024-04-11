115cda8c3ca3a0cf373cda44b0a388255ddc0781
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2024-04-10 19:15:25 -0700

    Update start.js

--- start.js
+++ start.js
@@ -1,18 +1,18 @@
+import { libcurlPath } from "@mercuryworkshop/libcurl-transport";
 import express from "express";
 import { createServer } from "node:http";
+import { publicPath } from "ultraviolet-static";
 import { uvPath } from "@titaniumnetwork-dev/ultraviolet";
 import { epoxyPath } from "@mercuryworkshop/epoxy-transport";
 import { baremuxPath } from "@mercuryworkshop/bare-mux";
 import { join } from "node:path";
 import { hostname } from "node:os";
-import { libcurlPath } from "@mercuryworkshop/libcurl-transport";
 import wisp from "wisp-server-node"
 import { rateLimit } from 'express-rate-limit'
 
-
 const app = express();
 // Load our publicPath first and prioritize it over UV.
-app.use(express.static('/'));
+app.use(express.static(publicPath));
 // Load vendor files last.
 // The vendor's uv.config.js won't conflict with our uv.config.js inside the publicPath directory.
 app.use("/uv/", express.static(uvPath));
@@ -30,6 +30,7 @@ const limiter = rateLimit({
 
 // Apply the rate limiting middleware to all requests
 app.use(limiter)
+
 // Error for everything else
 app.use((req, res) => {
   res.status(404);
@@ -54,7 +55,6 @@ let port = parseInt(process.env.PORT || "");
 
 if (isNaN(port)) port = 8080;
 
-
 server.on("listening", () => {
   const address = server.address();
 
@@ -64,8 +64,7 @@ server.on("listening", () => {
   console.log(`\thttp://localhost:${address.port}`);
   console.log(`\thttp://${hostname()}:${address.port}`);
   console.log(
-    `\thttp://${
-      address.family === "IPv6" ? `[${address.address}]` : address.address
+    `\thttp://${address.family === "IPv6" ? `[${address.address}]` : address.address
     }:${address.port}`
   );
 });

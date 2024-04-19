e575a1983246f7d4d060221a0dd0325f7749592a
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2024-04-18 19:09:15 -0700

    Update start.js

--- start.js
+++ start.js
@@ -1,6 +1,4 @@
-import { libcurlPath } from "@mercuryworkshop/libcurl-transport";
-import express from "express";
-import { rateLimit } from 'express-rate-limit'
+
 import express from "express";
 import { createServer } from "node:http";
 import { publicPath } from "ultraviolet-static";
@@ -12,23 +10,15 @@ import { hostname } from "node:os";
 import wisp from "wisp-server-node"
 
 const app = express();
-
+// Load our publicPath first and prioritize it over UV.
 app.use(express.static(publicPath));
-
+// Load vendor files last.
+// The vendor's uv.config.js won't conflict with our uv.config.js inside the publicPath directory.
 app.use("/uv/", express.static(uvPath));
 app.use("/epoxy/", express.static(epoxyPath));
 app.use("/baremux/", express.static(baremuxPath));
-app.use("/libcurl/", express.static(libcurlPath));
 
-const limiter = rateLimit({
-	windowMs: 15 * 60 * 1000,
-	limit: 100,
-	standardHeaders: 'draft-7',
-	legacyHeaders: false,
-})
-
-app.use(limiter)
-
+// Error for everything else
 app.use((req, res) => {
   res.status(404);
   res.sendFile(join(publicPath, "404.html"));
@@ -54,6 +44,9 @@ if (isNaN(port)) port = 8080;
 
 server.on("listening", () => {
   const address = server.address();
+
+  // by default we are listening on 0.0.0.0 (every interface)
+  // we just need to list a few
   console.log("Listening on:");
   console.log(`\thttp://localhost:${address.port}`);
   console.log(`\thttp://${hostname()}:${address.port}`);
@@ -63,3 +56,16 @@ server.on("listening", () => {
   );
 });
 
+// https://expressjs.com/en/advanced/healthcheck-graceful-shutdown.html
+process.on("SIGINT", shutdown);
+process.on("SIGTERM", shutdown);
+
+function shutdown() {
+  console.log("SIGTERM signal received: closing HTTP server");
+  server.close();
+  process.exit(0);
+}
+
+server.listen({
+  port,
+});

3803277a7d6efd791bd8a605753203ce124805eb
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2024-04-18 18:48:08 -0700

    Update uv-sw.js

--- uv-sw.js
+++ uv-sw.js
@@ -1,10 +1,10 @@
-importScripts('https://aluu.xyz/epoxy/index.js');
-importScripts("baremux/bare.cjs")
+//importScripts('https://aluu.xyz/epoxy/index.js');
+//importScripts("https://aluu.xyz/baremux/bare.cjs")
 importScripts("/uv/uv.bundle.js");
 importScripts("/uv/uv.sw.js");
-importScripts("https://aluu.xyz/libcurl/index.js");
+//importScripts("https://aluu.xyz/libcurl/index.js");
 importScripts(__uv$config.sw);
-importScripts("https://aluu.xyz/bare_transport.js");
+//importScripts("https://aluu.xyz/bare_transport.js");
 
 const sw = new UVServiceWorker();
 

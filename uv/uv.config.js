b8b77bdf890cfcc6777148110a967c9f2b2be278
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2024-04-18 20:55:30 -0700

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -4,10 +4,11 @@ self.__uv$config = {
     bare:  'https://bare.benrogo.net',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: 'uv/uv.handler.js',
-    bundle: 'uv/uv.bundle.js',
-    config: 'uv/uv.config.js',
-    sw: 'uv/uv.sw.js',
+    handler: "uv/uv.handler.js",
+    client: "uv/uv.client.js",
+    bundle: "uv/uv.bundle.js",
+    config: "uv/uv.config.js",
+    sw: "uv/uv.sw.js",
     inject: async (url) => {
        if (url.host === 'discord.com') {
            return `

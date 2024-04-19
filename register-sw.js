bb0078bb039ae9f9cb4f214cbebdef40b329ad4b
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2024-04-18 18:42:45 -0700

    Update register-sw.js

--- register-sw.js
+++ register-sw.js
@@ -5,7 +5,7 @@
 const stockSW = "/uv-sw.js";
 
 /**
- * List of hostnames that are allowed to run serviceworkers on http:
+ * List of hostnames that are allowed to run serviceworkers on http://
  */
 const swAllowedHostnames = ["localhost", "127.0.0.1"];
 
@@ -14,21 +14,22 @@ const swAllowedHostnames = ["localhost", "127.0.0.1"];
  * Used in 404.html and index.html
  */
 async function registerSW() {
-  if (
-    location.protocol !== "https:" &&
-    !swAllowedHostnames.includes(location.hostname)
-  )
-    throw new Error("Service workers cannot be registered without https.");
+  if (!navigator.serviceWorker) {
+    if (
+      location.protocol !== "https:" &&
+      !swAllowedHostnames.includes(location.hostname)
+    )
+      throw new Error("Service workers cannot be registered without https.");
 
-  if (!navigator.serviceWorker)
     throw new Error("Your browser doesn't support service workers.");
+  }
 
-  // Ultraviolet has a stock `sw.js` script.
   await navigator.serviceWorker.register(stockSW, {
     scope: __uv$config.prefix,
   });
 
   // Register the EpoxyClient transport to be used for network requests
-  let wispUrl = (location.protocol === "https:" ? "wss" : "ws") + "://" + location.host + "/wisp/";
+  //let wispUrl = (location.protocol === "https:" ? "wss" : "ws") + "://" + location.host + "/wisp/";
+  let wispUrl = "wss://tomp.app/wisp/"
   BareMux.SetTransport("EpxMod.EpoxyClient", { wisp: wispUrl });
 }

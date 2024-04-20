21736870221bba57329f6bb5ec19bb07d24f7fcd
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2024-04-20 16:54:26 -0700

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -2,8 +2,8 @@
 self.__uv$config = {
     prefix: '/uv/service/',
     bare: "https://bare.benrogo.net",
-    encodeUrl: Ultraviolet.codec.xor.encode,
-    decodeUrl: Ultraviolet.codec.xor.decode,
+    encodeUrl: Ultraviolet.codec.plain.encode,
+    decodeUrl: Ultraviolet.codec.plain.decode,
     handler: "uv/uv.handler.js",
     bundle: "uv/uv.bundle.js",
     config: "uv/uv.config.js",

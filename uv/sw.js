bdcc214cf345f5a12ccfbef9b451761af99e403d
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2024-04-19 20:56:45 -0700

    Update sw.js

--- uv/sw.js
+++ uv/sw.js
@@ -1,10 +1,3 @@
-/*global UVServiceWorker,__uv$config*/
-/*
- * Stock service worker script.
- * Users can provide their own sw.js if they need to extend the functionality of the service worker.
- * Ideally, this will be registered under the scope in uv.config.js so it will not need to be modified.
- * However, if a user changes the location of uv.bundle.js/uv.config.js or sw.js is not relative to them, they will need to modify this script locally.
- */
 importScripts('../epoxy/index.js');
 importScripts('/uv/uv.bundle.js');
 importScripts('/uv/uv.config.js');
@@ -12,8 +5,8 @@ importScripts('uv.sw.js');
 importScripts('/dynamic/dynamic.config.js');
 importScripts('/dynamic/dynamic.worker.js');
 
-const suvw = new UVServiceWorker();
-const dynamic = new Dynamic();
+const uv = new UVServiceWorker()
+const dynamic = new Dynamic()
 
 self.addEventListener('fetch', (event) => event.respondWith(sw.fetch(event)));
 

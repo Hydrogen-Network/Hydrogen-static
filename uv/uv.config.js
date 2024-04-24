08ecd5ce9ca6ad93791412c685c860bdd9a29b65
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2024-04-23 20:57:45 -0700

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,25 +1,5 @@
-/*global Ultraviolet*/
-self.__uv$config = {
-    prefix: '/service/uv/',
-    bare: "https://bare.benrogo.net",
-    encodeUrl: Ultraviolet.codec.plain.encode,
-    decodeUrl: Ultraviolet.codec.plain.decode,
-    handler: "uv/uv.handler.js",
-    bundle: "uv/uv.bundle.js",
-    config: "uv/uv.config.js",
-    sw: "uv/uv.sw.js",
-    inject: async (url) => {
-       if (url.host === 'discord.com') {
-           return `
-               <script src="https://raw.githubusercontent.com/Vencord/builds/main/browser.js"></script>
-               <link rel="stylesheet" href="https://raw.githubusercontent.com/Vencord/builds/main/browser.css">
-             `;
-        }
+let bare;
 
-       return ``;
-    },
-};
-/*
 function checkServerStatus(url) {
     const startTime = performance.now();
     
@@ -38,20 +18,44 @@ function checkServerStatus(url) {
 }
 
 if(checkServerStatus(localStorage.getItem("bareServer")) == true) {
-    self.__uv$config.bare = localStorage.getItem("bareServer");
+    bare = localStorage.getItem("bareServer");
 } else if(checkServerStatus("https://server.flow-works.me/bare/") == true) {
-    self.__uv$config.bare = "https://server.flow-works.me/bare/";
+    bare = "https://server.flow-works.me/bare/";
 } else if(checkServerStatus("https://bare.benrogo.net") == true) {
-    self.__uv$config.bare = "https://bare.benrogo.net";
+    bare = "https://bare.benrogo.net";
 } else if(checkServerStatus("https://worker-holy-smoke-5eb3.pres1234569.workers.dev/") == true) {
-    self.__uv$config.bare = "https://worker-holy-smoke-5eb3.pres1234569.workers.dev/";
+    bare = "https://worker-holy-smoke-5eb3.pres1234569.workers.dev/";
 } else if(checkServerStatus("https://uv.radon.games/bare1/") == true) {
-    self.__uv$config.bare = "https://uv.radon.games/bare1/";
+    bare = "https://uv.radon.games/bare1/";
 } else if(checkServerStatus("https://uv.radon.games/bare2/") == true) {
-    self.__uv$config.bare = "https://uv.radon.games/bare2/";
+    bare = "https://uv.radon.games/bare2/";
 } else if(checkServerStatus("https://uv.radon.games/bare3/") == true) {
-    self.__uv$config.bare = "https://uv.radon.games/bare3/";
+    bare = "https://uv.radon.games/bare3/";
 } else if(checkServerStatus("https://uv.radon.games/bare4/") == true) {
-    self.__uv$config.bare = "https://uv.radon.games/bare4/";
+    bare = "https://uv.radon.games/bare4/";
 }
-*/
+
+
+
+/*global Ultraviolet*/
+self.__uv$config = {
+    prefix: '/service/uv/',
+    bare: bare,
+    encodeUrl: Ultraviolet.codec.plain.encode,
+    decodeUrl: Ultraviolet.codec.plain.decode,
+    handler: "uv/uv.handler.js",
+    bundle: "uv/uv.bundle.js",
+    config: "uv/uv.config.js",
+    sw: "uv/uv.sw.js",
+    inject: async (url) => {
+       if (url.host === 'discord.com') {
+           return `
+               <script src="https://raw.githubusercontent.com/Vencord/builds/main/browser.js"></script>
+               <link rel="stylesheet" href="https://raw.githubusercontent.com/Vencord/builds/main/browser.css">
+             `;
+        }
+
+       return ``;
+    },
+};
+

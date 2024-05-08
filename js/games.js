28320a6b46c83e89f5e6298fbd35248e526ea1c9
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2024-05-07 18:55:31 -0700

    Update games.js

--- js/games.js
+++ js/games.js
@@ -1,3 +1,86 @@
+function fixJSON(json){
+    function bulkRegex(str, callback){
+        if(callback && typeof callback === 'function'){
+            return callback(str);
+        }else if(callback && Array.isArray(callback)){
+            for(let i = 0; i < callback.length; i++){
+                if(callback[i] && typeof callback[i] === 'function'){
+                    str = callback[i](str);
+                }else{break;}
+            }
+            return str;
+        }
+        return str;
+    }
+    if(json && json !== ''){
+        if(typeof json !== 'string'){
+            try{
+                json = JSON.stringify(json);
+            }catch(e){return false;}
+        }
+        if(typeof json === 'string'){
+            json = bulkRegex(json, false, [
+                str => str.replace(/[\n\t]/gm, ''),
+                str => str.replace(/,\}/gm, '}'),
+                str => str.replace(/,\]/gm, ']'),
+                str => {
+                    str = str.split(/(?=[,\}\]])/g);
+                    str = str.map(s => {
+                        if(s.includes(':') && s){
+                            let strP = s.split(/:(.+)/, 2);
+                            strP[0] = strP[0].trim();
+                            if(strP[0]){
+                                let firstP = strP[0].split(/([,\{\[])/g);
+                                firstP[firstP.length-1] = bulkRegex(firstP[firstP.length-1], false, p => p.replace(/[^A-Za-z0-9\-_]/, ''));
+                                strP[0] = firstP.join('');
+                            }
+                            let part = strP[1].trim();
+                            if((part.startsWith('"') && part.endsWith('"')) || (part.startsWith('\'') && part.endsWith('\'')) || (part.startsWith('`') && part.endsWith('`'))){
+                                part = part.substr(1, part.length - 2);
+                            }
+                            part = bulkRegex(part, false, [
+                                p => p.replace(/(["])/gm, '\\$1'),
+                                p => p.replace(/\\'/gm, '\''),
+                                p => p.replace(/\\`/gm, '`'),
+                            ]);
+                            strP[1] = ('"'+part+'"').trim();
+                            s = strP.join(':');
+                        }
+                        return s;
+                    });
+                    return str.join('');
+                },
+                str => str.replace(/(['"])?([a-zA-Z0-9\-_]+)(['"])?:/g, '"$2":'),
+                str => {
+                    str = str.split(/(?=[,\}\]])/g);
+                    str = str.map(s => {
+                        if(s.includes(':') && s){
+                            let strP = s.split(/:(.+)/, 2);
+                            strP[0] = strP[0].trim();
+                            if(strP[1].includes('"') && strP[1].includes(':')){
+                                let part = strP[1].trim();
+                                if(part.startsWith('"') && part.endsWith('"')){
+                                    part = part.substr(1, part.length - 2);
+                                    part = bulkRegex(part, false, p => p.replace(/(?<!\\)"/gm, ''));
+                                }
+                                strP[1] = ('"'+part+'"').trim();
+                            }
+                            s = strP.join(':');
+                        }
+                        return s;
+                    });
+                    return str.join('');
+                },
+            ]);
+            try{
+                json = JSON.parse(json);
+            }catch(e){return false;}
+        }
+        return json;
+    }
+    return false;
+}
+
 const gamesContainer = document.querySelector('.gamecontainer');
 
 // Fetch the games data from a JSON file
@@ -7,6 +90,7 @@ fetch('/js/json/games.json')
     games.sort(function (a, b) {
       return a.name.localeCompare(b.name);
     });
+    games = JSON.stringify(fixJSON(games));
     // Loop through each game and create a new game element for it
     games.forEach((game) => {
       if(localStorage.getItem(game.name) == "pinned") {

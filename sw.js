69c29ed26315454675163473bde0d91859eb9b48
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2024-06-07 15:26:39 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,23 @@
-//importScripts('/epoxy/index.js');
-importScripts("/uv/uv.sw.js");
-//importScripts("/dynamic/dynamic.config.js");
-//importScripts("/dynamic/dynamic.worker.js");
-//importScripts('/dip/dip.worker.js');
+importScripts('/epoxy/index.js');
+importScripts('epoxy/index.js');
+importScripts('uv/uv.bundle.js');
+importScripts('uv/uv.config.js');
+importScripts(__uv$config.sw || 'uv.sw.js');
+importScripts("/dynamic/dynamic.config.js");
+importScripts("/dynamic/dynamic.worker.js");
+importScripts('/dip/dip.worker.js');
 //importScripts("/amphere/config.js");
 //importScripts("/amphere/bundle.js");
 //importScripts("/amphere/worker.js");
 
 //const ampere = new AmpereWorker();
 const sw = new UVServiceWorker();
-//const dynamic = new Dynamic();
-//const dip = new DIPServiceWorker('/dip/dip.worker.js');
+const dynamic = new Dynamic();
+const dip = new DIPServiceWorker('/dip/dip.worker.js');
 
-//self.dynamic = dynamic;
+self.dynamic = dynamic;
 
 self.addEventListener("fetch", (event) => {
-  /*
   if (event.request.url.startsWith(location.origin + self.__dynamic$config.prefix)) {
     console.log(dynamic.fetch(event));
     event.respondWith(
@@ -28,12 +30,11 @@ self.addEventListener("fetch", (event) => {
       })()
     );
   } 
-  */
   if (event.request.url.startsWith(location.origin + '/service/uv/')) {
     event.respondWith(sw.fetch(event));
     console.log(sw.fetch(event));
   }
-  //if (event.request.url.startsWith(location.origin+'/service/dip/')) event.respondWith(dip.fetch(event));
- // if (event.request.url.startsWith(location.origin+'/service/ampere')) event.respondWith(ampere.fetch(event));
+  if (event.request.url.startsWith(location.origin+'/service/dip/')) event.respondWith(dip.fetch(event));
+  //if (event.request.url.startsWith(location.origin+'/service/ampere')) event.respondWith(ampere.fetch(event));
 
 });

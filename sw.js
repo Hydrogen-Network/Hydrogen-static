cd58d1e95c652d6859f5ec6b377968b5a6bde8f3
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2024-06-07 14:22:12 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,11 +3,11 @@ importScripts("/uv/uv.sw.js");
 importScripts("/dynamic/dynamic.config.js");
 importScripts("/dynamic/dynamic.worker.js");
 importScripts('/dip/dip.worker.js');
-importScripts("/amphere/config.js");
-importScripts("/amphere/bundle.js");
-importScripts("/amphere/worker.js");
+//importScripts("/amphere/config.js");
+//importScripts("/amphere/bundle.js");
+//importScripts("/amphere/worker.js");
 
-const ampere = new AmpereWorker();
+//const ampere = new AmpereWorker();
 const sw = new UVServiceWorker();
 const dynamic = new Dynamic();
 const dip = new DIPServiceWorker('/dip/dip.worker.js');
@@ -32,6 +32,6 @@ self.addEventListener("fetch", (event) => {
     console.log(sw.fetch(event));
   }
   if (event.request.url.startsWith(location.origin+'/service/dip/')) event.respondWith(dip.fetch(event));
-  if (event.request.url.startsWith(location.origin+'/service/ampere')) event.respondWith(ampere.fetch(event));
+ // if (event.request.url.startsWith(location.origin+'/service/ampere')) event.respondWith(ampere.fetch(event));
 
 });

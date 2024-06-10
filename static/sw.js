e426a7d3a4c66dfd149507c8b061f07cd764c886
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2024-06-10 11:03:31 -0700

    Update sw.js

--- static/sw.js
+++ static/sw.js
@@ -1,6 +1,7 @@
 importScripts('assets/epoxy/index.js');
 importScripts('assets/uv/uv.bundle.js');
 importScripts('assets/uv/uv.config.js');
+importScripts('assets/libcurl/libcurl.js');
 importScripts(__uv$config.sw || 'assets/uv/uv.sw.js');
 
 const sw = new UVServiceWorker();

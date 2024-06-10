4239c0edd73f6303706c9743196767bb51a98f7f
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2024-06-10 10:50:15 -0700

    Create sw.js

---
+++ static/sw.js
@@ -0,0 +1,8 @@
+importScripts('assets/epoxy/index.js');
+importScripts('assets/uv/uv.bundle.js');
+importScripts('assets/uv/uv.config.js');
+importScripts(__uv$config.sw || 'assets/uv/uv.sw.js');
+
+const sw = new UVServiceWorker();
+
+self.addEventListener('fetch', (event) => event.respondWith(sw.fetch(event)));

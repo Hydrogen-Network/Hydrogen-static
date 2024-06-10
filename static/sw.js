31f433ac167571cd0f6872c36e52b79ddabbc007
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2024-06-10 16:52:40 -0700

    Update sw.js

--- static/sw.js
+++ static/sw.js
@@ -1,9 +1,24 @@
-importScripts('assets/epoxy/index.js');
-importScripts('assets/uv/uv.bundle.js');
-importScripts('assets/uv/uv.config.js');
-importScripts('assets/libcurl/index.js');
-importScripts(__uv$config.sw || 'assets/uv/uv.sw.js');
+importScripts("/dynamic/dynamic.config.js")
+importScripts("/assets/dynamic/dynamic.worker.js")
+importScripts("/assets/uv/uv.bundle.js")
+importScripts("/assets/uv/uv.config.js")
+importScripts(__uv$config.sw || "/assets/uv/uv.sw.js")
 
-const sw = new UVServiceWorker();
+const uv = new UVServiceWorker()
+const dynamic = new Dynamic()
 
-self.addEventListener('fetch', (event) => event.respondWith(sw.fetch(event)));
+self.dynamic = dynamic
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+      if (await dynamic.route(event)) {
+        return await dynamic.fetch(event)
+      }
+
+      if (event.request.url.startsWith(location.origin + "/uv/service/")) {
+        return await uv.fetch(event)
+      }
+
+      return await fetch(event.request)
+  )
+})
